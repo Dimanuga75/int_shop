5143f1de9309504bed81779f08f1625a85c5514b
Dima Grigoryev <dimanuga75@gmail.com>
2022-10-11 15:41:22 +0800

    commit

--- dist/files/basket.js
+++ dist/files/basket.js
@@ -1,108 +1,108 @@
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-  smoothLink.addEventListener("click", function (e) {
-    e.preventDefault();
-    const id = smoothLink.getAttribute("href");
+//const smoothLinks = document.querySelectorAll('a[href^="#"]');
+//for (let smoothLink of smoothLinks) {
+//  smoothLink.addEventListener("click", function (e) {
+//    e.preventDefault();
+//    const id = smoothLink.getAttribute("href");
 
-    document.querySelector(id).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
-}
-let btnCatalogMenu = document.querySelector(".menu-mobile__btn");
-let headerCatalog = document.querySelector(".header-catalog__menu");
-let wrapperHeaderCatalog = document.querySelector(".wrapper__headder-catalog");
-let closeHeaderCatalog = document.querySelector(".header-catalog__close");
-const mQuery767 = window.matchMedia("(max-width: 767.98px)");
-const mQuery1200 = window.matchMedia("(max-width: 1199.98px)");
-const mQuery991 = window.matchMedia("(max-width: 991.98px)");
-const mQuery479 = window.matchMedia("(max-width: 479.98px)");
+//    document.querySelector(id).scrollIntoView({
+//      behavior: "smooth",
+//      block: "start",
+//    });
+//  });
+//}
+//let btnCatalogMenu = document.querySelector(".menu-mobile__btn");
+//let headerCatalog = document.querySelector(".header-catalog__menu");
+//let wrapperHeaderCatalog = document.querySelector(".wrapper__headder-catalog");
+//let closeHeaderCatalog = document.querySelector(".header-catalog__close");
+//const mQuery767 = window.matchMedia("(max-width: 767.98px)");
+//const mQuery1200 = window.matchMedia("(max-width: 1199.98px)");
+//const mQuery991 = window.matchMedia("(max-width: 991.98px)");
+//const mQuery479 = window.matchMedia("(max-width: 479.98px)");
 
-btnCatalogMenu.addEventListener("click", function (e) {
-  e.preventDefault();
-  wrapperHeaderCatalog.style.visibility = "visible";
-  headerCatalog.style.transform = "translateX(0%)";
-  headerCatalog.style.visibility = "visible";
-});
-closeHeaderCatalog.addEventListener("click", function (e) {
-  e.preventDefault();
-  wrapperHeaderCatalog.style.visibility = "hidden";
-  headerCatalog.style.transform = "translateX(-150%)";
-  headerCatalog.style.visibility = "hidden";
-});
+//btnCatalogMenu.addEventListener("click", function (e) {
+//  e.preventDefault();
+//  wrapperHeaderCatalog.style.visibility = "visible";
+//  headerCatalog.style.transform = "translateX(0%)";
+//  headerCatalog.style.visibility = "visible";
+//});
+//closeHeaderCatalog.addEventListener("click", function (e) {
+//  e.preventDefault();
+//  wrapperHeaderCatalog.style.visibility = "hidden";
+//  headerCatalog.style.transform = "translateX(-150%)";
+//  headerCatalog.style.visibility = "hidden";
+//});
 
-let categoryItems = document.querySelector(".header-catalog__box-category");
-let categoryItem = document.querySelectorAll(".header-category__item");
-let categoryBraket = document.querySelectorAll(".header-category__bracket");
-let categoryPoint = document.querySelectorAll(".header-catalog__box-item");
-let categoryBox = document.querySelector(".header-catalog__box-items");
+//let categoryItems = document.querySelector(".header-catalog__box-category");
+//let categoryItem = document.querySelectorAll(".header-category__item");
+//let categoryBraket = document.querySelectorAll(".header-category__bracket");
+//let categoryPoint = document.querySelectorAll(".header-catalog__box-item");
+//let categoryBox = document.querySelector(".header-catalog__box-items");
 
-if (mQuery767.matches) {
-  for (const item of categoryItem) {
-    item.classList.remove("active");
-  }
-  for (const item of categoryBraket) {
-    item.classList.remove("active");
-  }
-  for (const item of categoryPoint) {
-    item.classList.remove("active");
-    item.insertAdjacentHTML(
-      "beforeend",
-      " <button  class='return__menu-mobile'> &#10229; &nbsp; Вернуться в меню</button>"
-    );
-  }
-  categoryItems.insertAdjacentHTML(
-    "beforeend",
-    "<div class='header__menu-add'> <a href='#''>Мероприятия</a><a href='#''>Блог</a><a href='#''>О центре</a> <a href='#''>Контакты</a> </div>"
-  );
-  categoryBox.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (e.target.classList.contains("return__menu-mobile")) {
-      for (const item of categoryPoint) {
-        item.classList.remove("active");
-      }
-    }
-  });
-}
-const headerCatalogVisible = function (e) {
-  if (e.target.classList.contains("click")) {
-    e.preventDefault();
-    for (const item of categoryItem) {
-      item.classList.remove("active");
-    }
-    e.target.classList.add("active");
-    e.target.parentElement.classList.add("active");
-    const index = e.target.dataset.category;
+//if (mQuery767.matches) {
+//  for (const item of categoryItem) {
+//    item.classList.remove("active");
+//  }
+//  for (const item of categoryBraket) {
+//    item.classList.remove("active");
+//  }
+//  for (const item of categoryPoint) {
+//    item.classList.remove("active");
+//    item.insertAdjacentHTML(
+//      "beforeend",
+//      " <button  class='return__menu-mobile'> &#10229; &nbsp; Вернуться в меню</button>"
+//    );
+//  }
+//  categoryItems.insertAdjacentHTML(
+//    "beforeend",
+//    "<div class='header__menu-add'> <a href='#''>Мероприятия</a><a href='#''>Блог</a><a href='#''>О центре</a> <a href='#''>Контакты</a> </div>"
+//  );
+//  categoryBox.addEventListener("click", function (e) {
+//    e.preventDefault();
+//    if (e.target.classList.contains("return__menu-mobile")) {
+//      for (const item of categoryPoint) {
+//        item.classList.remove("active");
+//      }
+//    }
+//  });
+//}
+//const headerCatalogVisible = function (e) {
+//  if (e.target.classList.contains("click")) {
+//    e.preventDefault();
+//    for (const item of categoryItem) {
+//      item.classList.remove("active");
+//    }
+//    e.target.classList.add("active");
+//    e.target.parentElement.classList.add("active");
+//    const index = e.target.dataset.category;
 
-    for (const item of categoryBraket) {
-      item.classList.remove("active");
-    }
-    categoryBraket[index].classList.add("active");
+//    for (const item of categoryBraket) {
+//      item.classList.remove("active");
+//    }
+//    categoryBraket[index].classList.add("active");
 
-    for (const item of categoryPoint) {
-      item.classList.remove("active");
-    }
-    categoryPoint[index].classList.add("active");
-  }
-};
-categoryItems.addEventListener("click", headerCatalogVisible);
-let mobileBtnMenu = document.querySelector(".catalog-mobile__box");
-let headerTop = document.querySelector(".header__content");
+//    for (const item of categoryPoint) {
+//      item.classList.remove("active");
+//    }
+//    categoryPoint[index].classList.add("active");
+//  }
+//};
+//categoryItems.addEventListener("click", headerCatalogVisible);
+//let mobileBtnMenu = document.querySelector(".catalog-mobile__box");
+//let headerTop = document.querySelector(".header__content");
 
-if (mQuery767.matches) {
-  headerTop.prepend(mobileBtnMenu);
-}
-let footerAddress = document.querySelector(".footer__address");
-let footerRules = document.querySelector(".footer__rules-list");
-if (mQuery767.matches) {
-  footerRules.insertAdjacentElement("afterend", footerAddress);
-}
-let politicalCopy = document.querySelector(".political__copy");
-let ofertaLink = document.querySelector(".oferta__link");
-if (mQuery767.matches) {
-  ofertaLink.insertAdjacentElement("afterend", politicalCopy);
-}
+//if (mQuery767.matches) {
+//  headerTop.prepend(mobileBtnMenu);
+//}
+//let footerAddress = document.querySelector(".footer__address");
+//let footerRules = document.querySelector(".footer__rules-list");
+//if (mQuery767.matches) {
+//  footerRules.insertAdjacentElement("afterend", footerAddress);
+//}
+//let politicalCopy = document.querySelector(".political__copy");
+//let ofertaLink = document.querySelector(".oferta__link");
+//if (mQuery767.matches) {
+//  ofertaLink.insertAdjacentElement("afterend", politicalCopy);
+//}
 
 const sumOrder = function () {
   let priceItemBasket = document.querySelectorAll(".product__price-basket");
--- dist/files/catalog.js
+++ dist/files/catalog.js
@@ -1,112 +1,3 @@
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (let smoothLink of smoothLinks) {
-  smoothLink.addEventListener("click", function (e) {
-    e.preventDefault();
-    const id = smoothLink.getAttribute("href");
-
-    document.querySelector(id).scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  });
-}
-
-let btnCatalogMenu = document.querySelector(".menu-mobile__btn");
-let headerCatalog = document.querySelector(".header-catalog__menu");
-let wrapperHeaderCatalog = document.querySelector(".wrapper__headder-catalog");
-let closeHeaderCatalog = document.querySelector(".header-catalog__close");
-const mQuery767 = window.matchMedia("(max-width: 767.98px)");
-const mQuery1200 = window.matchMedia("(max-width: 1199.98px)");
-const mQuery991 = window.matchMedia("(max-width: 991.98px)");
-const mQuery479 = window.matchMedia("(max-width: 479.98px)");
-
-btnCatalogMenu.addEventListener("click", function (e) {
-  e.preventDefault();
-  wrapperHeaderCatalog.style.visibility = "visible";
-  headerCatalog.style.transform = "translateX(0%)";
-  headerCatalog.style.visibility = "visible";
-});
-closeHeaderCatalog.addEventListener("click", function (e) {
-  e.preventDefault();
-  wrapperHeaderCatalog.style.visibility = "hidden";
-  headerCatalog.style.transform = "translateX(-150%)";
-  headerCatalog.style.visibility = "hidden";
-});
-
-let categoryItems = document.querySelector(".header-catalog__box-category");
-let categoryItem = document.querySelectorAll(".header-category__item");
-let categoryBraket = document.querySelectorAll(".header-category__bracket");
-let categoryPoint = document.querySelectorAll(".header-catalog__box-item");
-let categoryBox = document.querySelector(".header-catalog__box-items");
-
-if (mQuery767.matches) {
-  for (const item of categoryItem) {
-    item.classList.remove("active");
-  }
-  for (const item of categoryBraket) {
-    item.classList.remove("active");
-  }
-  for (const item of categoryPoint) {
-    item.classList.remove("active");
-    item.insertAdjacentHTML(
-      "beforeend",
-      " <button  class='return__menu-mobile'> &#10229; &nbsp; Вернуться в меню</button>"
-    );
-  }
-  categoryItems.insertAdjacentHTML(
-    "beforeend",
-    "<div class='header__menu-add'> <a href='#''>Мероприятия</a><a href='#''>Блог</a><a href='#''>О центре</a> <a href='#''>Контакты</a> </div>"
-  );
-  categoryBox.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (e.target.classList.contains("return__menu-mobile")) {
-      for (const item of categoryPoint) {
-        item.classList.remove("active");
-      }
-    }
-  });
-}
-const headerCatalogVisible = function (e) {
-  if (e.target.classList.contains("click")) {
-    e.preventDefault();
-    for (const item of categoryItem) {
-      item.classList.remove("active");
-    }
-    e.target.classList.add("active");
-    e.target.parentElement.classList.add("active");
-    const index = e.target.dataset.category;
-
-    for (const item of categoryBraket) {
-      item.classList.remove("active");
-    }
-    categoryBraket[index].classList.add("active");
-
-    for (const item of categoryPoint) {
-      item.classList.remove("active");
-    }
-    categoryPoint[index].classList.add("active");
-  }
-};
-categoryItems.addEventListener("click", headerCatalogVisible);
-let mobileBtnMenu = document.querySelector(".catalog-mobile__box");
-let headerTop = document.querySelector(".header__content");
-
-if (mQuery767.matches) {
-  headerTop.prepend(mobileBtnMenu);
-}
-let footerAddress = document.querySelector(".footer__address");
-let footerRules = document.querySelector(".footer__rules-list");
-if (mQuery767.matches) {
-  footerRules.insertAdjacentElement("afterend", footerAddress);
-}
-let politicalCopy = document.querySelector(".political__copy");
-let ofertaLink = document.querySelector(".oferta__link");
-if (mQuery767.matches) {
-  ofertaLink.insertAdjacentElement("afterend", politicalCopy);
-}
-
-//-------------------------------------------------
-
 window.onload = function () {
   rangeOne();
   rangeTwo();
@@ -120,7 +11,6 @@ let categoryAll = document.querySelector(".category__inner");
 let categoryItemFirst = document.querySelector(".catalog-main__item-title");
 
 let categoryAllExpand = document.querySelector(".catalog-main__all-category");
-//let categoryItemMain = document.querySelector(".catalog-main__category-item");
 
 categoryMain.addEventListener("click", function (e) {
   if (e.target.classList.contains("catalog-main__all-category")) {
@@ -131,7 +21,6 @@ categoryMain.addEventListener("click", function (e) {
   let stepTop;
   if (e.target.classList.contains("catalog-main__item-title")) {
     e.preventDefault();
-    // let tablePlayWrapper = document.querySelector(".category-item__wrapper");
     categoryItemFirst.classList.toggle("expand--active");
     let categoryTablePlay = document.querySelector(
       ".catalog-main__category-item.table-play"
@@ -147,7 +36,6 @@ categoryMain.addEventListener("click", function (e) {
     stepTop = TablePlayheight + tablePlayTop;
 
     if (tablePlayTop === 10) {
-      //console.log("ttttttttttttt");
       categoryTablePlay.style.marginTop = `-${stepTop}px`;
     } else {
       categoryTablePlay.style.marginTop = "10px";
@@ -229,7 +117,6 @@ let filterPlayerTwo = document.querySelector(".aside-filter__player2");
 let minGapPlayer = 0;
 let rangePlayerTrack = document.querySelector(".aside-filter__player-track");
 let playerMaxValue = document.getElementById("player-1").max;
-//let rangeMinValue = document.getElementById("slider-2").min;
 
 function playerOne() {
   if (
@@ -238,7 +125,6 @@ function playerOne() {
   ) {
     filterPlayerOne.value = parseInt(filterPlayerTwo.value) - minGapPlayer;
   }
-  //  filterRangeDisplayOne.textContent =  filterPlayerOne.value;
   fiilColor1();
 }
 function playerTwo() {
@@ -264,7 +150,6 @@ function faqTextMarginTop() {
     );
   }
   for (let k = 0; k < faqTextHeigt.length; k++) {
-    // console.log(faqTextHeigt[k]);
     faqText[k].style.marginTop = `-${faqTextHeigt[k]}px`;
   }
 }
